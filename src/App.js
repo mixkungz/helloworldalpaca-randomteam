d28b695de9c9aac3736b72f3ac2911d56631b6fe
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-10-07 17:03:09 +0700

    add element + parallaxjs(not working)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Parallax from  'parallax-js';
 import styled , {injectGlobal} from 'styled-components'
+import mountainElement from './static/img/mountain.png';
 
 const MySpan = styled.span`
   font-size: 50px;
@@ -15,65 +14,77 @@ const MyFlex = styled.div`
   min-width: 100vw;
   min-height: 100vh;
 `
-
-const TwoFlex = MyFlex.extend`
-  color : ${props=> props.color}
+const Footer = styled.div`
+  position:absolute;
+  bottom:0;
+  max-width:100%;
+  left:0;
 `
-
-
+const Container = styled.div`
+  display:flex;
+  position:absolute;
+  min-width:100vw;
+  min-height:100vh;
+  top:0;
+  left:0;
+`
+const Mountain = styled.div`
+  background: url(${mountainElement});
+  max-width:15%;
+  min-width:100vw;
+  min-height:10vh;
+`
 injectGlobal`
-  .tae {
-    font-size: 90px;
+  body{
+    background:#e8f8fd;
+  }
+  img{
+    max-width:100%;
+  }
+  .alpaca{
+    max-width:20%;
   }
 `
-
-
 class App extends Component {
   
-  state = {
-    a : 's',
-    color : 'black',
-    check : true
+  componentWillUnmount() {
+    const scene = document.getElementById('scene');
+    const parallaxInstance = new Parallax(scene);
+    parallaxInstance.friction(0.2, 0.2);
+    
   }
+  componentDidMount() {
+    const scene = document.getElementById('scene');
+    const parallaxInstance = new Parallax(scene);
+    parallaxInstance.friction(0.2, 0.2);
+  }
+
+  state = {
+  }
+
 
   random = () =>{
-   console.log('www') 
   }
 
-  changeColor = () => {
-    if(this.state.check) {
-      this.setState({
-        color : 'red',
-
-      })
-    } else {
-        this.setState({
-          color : 'black',
-        })
-    }
-
-    this.setState({
-      check: !this.state.check
-    })
-  }
 
   render() {
     return (
-      <div>
-        <TwoFlex color={this.state.color} className="App">
-          <button onClick={()=>this.changeColor()}> Change Color </button>
-
-          <MySpan className="tae"> {this.state.a} </MySpan>
-
-          <span style={{ fontSize : '50px'}}> 
-            {this.state.a}
-          </span>
-
-        </TwoFlex>
-
+      <div data-relative-input="true" id="scene">
+        <Container data-relative-input="true" id="scene">
+          <Footer data-depth="0.6">
+            <img className="alpaca" data-depth="0.2" src={ require('./static/img/alpacamood-01.png') } />
+            <img className="alpaca" data-depth="0.2" src={ require('./static/img/alpacamood-05.png') } />
+            <img className="alpaca" data-depth="0.2" src={ require('./static/img/alpacamood-07.png') } />
+            <img className="alpaca" data-depth="0.2" src={ require('./static/img/alpacamood-04.png') } />
+            <Mountain/>
+            {/* <img src={`${mountainElement}`} alt=""/> */}
+          </Footer>
+        </Container>
       </div>
     );
   }
+
+
 }
 
 export default App;
